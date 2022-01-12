1aea18becbf1da74ee036815706bda39e6b812e3
Herison Almeida <herialmeidasantos@gmail.com>
2022-01-12 20:50:06 -0300

    ADD Comentário

--- src/main/java/com/project/professor/allocation/ProfessorAllocationApplication.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfessorAllocationApplication {
-
+// Inicializando Projeto...
 	public static void main(String[] args) {
 		SpringApplication.run(ProfessorAllocationApplication.class, args);
 	}
