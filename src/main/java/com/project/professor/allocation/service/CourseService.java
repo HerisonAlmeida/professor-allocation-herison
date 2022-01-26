74f89fd2037f0211b6208f0d9447e0faa5780f21
Herison Almeida <herialmeidasantos@gmail.com>
2022-01-26 20:39:25 -0300

    ADD Mod. Service

--- src/main/java/com/project/professor/allocation/service/CourseService.java
+++ src/main/java/com/project/professor/allocation/service/CourseService.java
@@ -1,7 +1,11 @@
 package com.project.professor.allocation.service;
 
+import com.project.professor.allocation.entity.Allocation;
+import com.project.professor.allocation.entity.Course;
 import com.project.professor.allocation.repository.CourseRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CourseService {
@@ -11,4 +15,12 @@ public class CourseService {
     public CourseService(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
     }
+
+    public Course findById(Long id) {
+        Optional<Course> courseOptional = courseRepository.findById(id);
+        Course course = courseOptional.orElse(null);
+        return course ;
+
 }
+}
+
--- src/main/java/com/project/professor/allocation/service/ProfessorService.java
+++ src/main/java/com/project/professor/allocation/service/ProfessorService.java
@@ -1,7 +1,10 @@
 package com.project.professor.allocation.service;
 
+import com.project.professor.allocation.entity.Professor;
 import com.project.professor.allocation.repository.ProfessorRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProfessorService {
@@ -9,6 +12,16 @@ public class ProfessorService {
     private ProfessorRepository professorRepository;
 
     public ProfessorService(ProfessorRepository professorRepository) {
+
         this.professorRepository = professorRepository;
+
+    }
+
+        public Professor findById(Long id)
+        {
+            Optional<Professor> professorOptional = professorRepository.findById(id);
+            Professor professor = professorOptional.orElse(null);
+            return professor;
+
     }
 }
