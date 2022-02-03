105aaaebb7a97de2ee6224a2c1a0fdaa89a76c84
Herison Almeida <herialmeidasantos@gmail.com>
2022-02-02 22:14:18 -0300

    ADD Mod. Service 2.0

--- src/main/java/com/project/professor/allocation/service/CourseService.java
+++ src/main/java/com/project/professor/allocation/service/CourseService.java
@@ -2,9 +2,11 @@ package com.project.professor.allocation.service;
 
 import com.project.professor.allocation.entity.Allocation;
 import com.project.professor.allocation.entity.Course;
+import com.project.professor.allocation.entity.Professor;
 import com.project.professor.allocation.repository.CourseRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,5 +24,47 @@ public class CourseService {
         return course ;
 
 }
+    public List<Course> findAll()
+    {
+        List<Course> course = courseRepository.findAll();
+        return course;
+    }
+
+    public Course create(Course course)
+    {
+        course.setId(null);
+      Course  coursenew = courseRepository.save(course);
+
+        return coursenew;
+    }
+
+    public Course update(Course course)
+    {
+        Long id = course.getId();
+
+        if (id != null && courseRepository.existsById(id))
+        {
+            Course  courseNew = courseRepository.save(course);
+            return courseNew;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void deleteById(Long id)
+    {
+        if (id != null && courseRepository.existsById(id))
+        {
+            courseRepository.deleteById(id);
+        }
+    }
+
+    public void deleteAll()
+    {
+        courseRepository.deleteAll();
+    }
+
 }
 
---
+++ src/test/java/com/project/professor/allocation/service/DepartmentServiceTest.java
@@ -0,0 +1,6 @@
+package com.project.professor.allocation.service;
+
+public class DepartmentServiceTest {
+
+
+}
